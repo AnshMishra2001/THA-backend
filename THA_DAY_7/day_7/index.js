a9f3e50e533df896a23a0a831d4f35d238a7e61f
Your Name <you@example.com>
2021-10-09 12:02:13 +0530

    Changes Added

--- THA_DAY_7/day_7/index.js
+++ THA_DAY_7/day_7/index.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const app = express();
 // CRUD :POST, GET, PUT, DELETE
-app.get('/', (req,res) =>{
-    res.status(500).send("Database not connecting");
-  } );
-// app.get('/products',(req,res) =>{
-//    req.query = {
-//        limit: 50,
-//        q: something
-//    }
-//    res.send(req.query.q); //converts into an object
-// });
-app.get('/ab?cd' , (req,res) => {
-    res.send("abcd");
-})
-app.get('/ab+cd' , (req,res) => {
-   res.send("abcd");
-})
-app.get('/user/:id/books/:bookId', (req, res) => {
+const checkAdmin = (req,res,next) => {
+  console.log("OLA ANDREA, VIVA MEXICO")
+  if(req.query.auth === 'abccd') next();
+  else res.status(200).send("Only admin access permitted")
+};
+const sendRes = (req,res) => {
+    res.status(200)
+    res.json(req.query)
+  };
+ 
+ // app.use(checkAdmin);
+ /*
+ app.get('/', (req,res) =>{
+     res.status(200).send("Database not connecting");
+   });
+*/
+app.post('/' , (req,res) => {
+  console.log('req-->body = ' , req.body);
+  res.json( { text: req.body })
+});
+ app.get('/products',(req,res) =>{
+ res.send(req.query.q); //converts into an object
+ })
+
+ app.get('/user/:userId/books/:bookId', (req,res) =>{
     console.log(req.query);
-    req.send(res.param.id);
-})
+    res.send(req.params.userId);
+});
+// app.get('/ab?cd' , (req,res) => {
+//     res.send("abcd");
+// })
+// app.get('/ab+cd' , (req,res) => {
+//    res.send("abcd");
+// })
+// app.get('/user/:id/books/:bookId', (req, res) => {
+//     console.log(req.query);
+//     req.send(res.param.id);
+// })
 app.listen(5000);
